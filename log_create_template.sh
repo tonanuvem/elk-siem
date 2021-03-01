23031277af32cacc4c2092b31de839ebc397475e
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2021-02-28 23:26:29 -0300

    Update log_create_template.sh

--- log_create_template.sh
+++ log_create_template.sh
@@ -12,10 +12,14 @@ PASSWORD=changeme
 
 printf "\n== Script for uploading template == \n \n"
 
-printf "\n== Uploading data to index... \n"
+printf "\n== Uploading template of index... \n"
 
 curl -s -u ${USERNAME}:${PASSWORD} -X PUT -H "Content-Type: application/json" ${URL}/_template/${INDEX_NAME}/ --data-binary ${TEMPLATE_FILE}
 
+printf "\n== Deleting index... \n"
+
+curl -XDELETE http://localhost:9200/${INDEX_NAME}?pretty
+
 printf "\n\n== Check upload \n"
 
-curl -XGET http://localhost:9200/ufw_logs/_mapping?pretty
+curl -XGET http://localhost:9200/${INDEX_NAME}/_mapping?pretty
