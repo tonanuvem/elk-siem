fbdf9051c1d51b0634ba51efc3eafab412652f1e
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2021-03-03 13:13:46 -0300

    Create run_ecommerce.sh

---
+++ run_ecommerce.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+## To create with security disabled remove -u username:password to curl commands
+
+HOST='localhost'
+PORT=9200
+INDEX_NAME='ecommerce'
+LOG_FILE='ecommerce.log'
+URL="http://${HOST}:${PORT}"
+USERNAME=elastic
+PASSWORD=changeme
+
+printf "\n== Script for creating index and uploading data == \n \n"
+
+#printf "\n== Deleting old index == \n\n"
+#curl -s -u ${USERNAME}:${PASSWORD} -X DELETE ${URL}/${INDEX_NAME}
+
+printf "\n== Creating Index - ${INDEX_NAME} == \n\n"
+
+tar -zxvf ecommerce.tgz
+sed -i 's|2019-08|2020-02|' $LOG_FILE
+
+tail $LOG_FILE
